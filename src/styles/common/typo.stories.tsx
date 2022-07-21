cacf10d727579825ad542b4f15ae736c250daaff
yoonminsang <apdlyooapdl@naver.com>
2022-07-22 02:17:11 +0900

    feat: 공통스타일(typo) 추가

---
+++ src/styles/common/typo.stories.tsx
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { TypoStyle } from './typo.style';
+
+const Div = styled.div``;
+
+export default {
+  title: 'styles/common/typo',
+  component: Div,
+} as ComponentMeta<typeof Div>;
+
+const Template: ComponentStory<typeof Div> = (args) => <Div {...args} />;
+
+export const Default = () => (
+  <>
+    {Object.entries(TypoStyle).map(([key, value]) => (
+      <Template key={key} css={value}>
+        {key}
+      </Template>
+    ))}
+  </>
+);
