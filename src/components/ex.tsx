c17b4c4d6004327acf8cf8177776904af2a3da7e
yoonminsang <apdlyooapdl@naver.com>
2022-07-23 19:42:37 +0900

    feat: 연습용 플로팅 컴포넌트 생성

---
+++ src/components/ex.tsx
@@ -0,0 +1,80 @@
+import { Link } from 'react-router-dom';
+import styled from '@emotion/styled';
+import { FC, useState } from 'react';
+
+import { URL } from '@/utils/common';
+
+const ExWrapper = styled.nav`
+  position: fixed;
+  top: 10px;
+  right: 10px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+
+  height: 100vh;
+
+  overflow: scroll;
+
+  ${(props) => props.theme.scroll}
+
+  button {
+    color: ${(props) => props.theme.colorGreen70};
+  }
+
+  a {
+    color: ${(props) => props.theme.colorGreen60};
+    background-color: ${(props) => props.theme.colorGreen10};
+    border-radius: 10px;
+    padding: 10px;
+  }
+`;
+
+const Ex: FC = () => {
+  const [isShow, setIsShow] = useState<boolean>(false);
+
+  return (
+    <ExWrapper>
+      <button type="button" onClick={() => setIsShow((isShow) => !isShow)}>
+        {isShow ? '가리기' : '보여주기'}
+      </button>
+      {isShow && (
+        <>
+          <Link to={`${URL}`}>홈</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+          <Link to={`${URL.ex['error-boundary']}`}>에러 바운더리</Link>
+        </>
+      )}
+    </ExWrapper>
+  );
+};
+
+export default Ex;
