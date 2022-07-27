47936b3b6303689b6cd3268276a3b3052ed22728
yoonminsang <apdlyooapdl@naver.com>
2022-07-28 00:32:42 +0900

    feat: react query 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import * as ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@emotion/react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { ToastContainer } from 'react-toastify';
+import { QueryClient, QueryClientProvider, QueryErrorResetBoundary } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from './app';
 import { ToastStyle } from './styles/toast.style';
@@ -13,13 +15,21 @@ import ErrorBoundaryPage from './pages/error/error-boundary';
 ReactDOM.createRoot(window.document.getElementById('root') as HTMLElement).render(
   <ThemeProvider theme={theme}>
     <GlobalStyle />
-    <ErrorBoundary
-      fallbackRender={({ resetErrorBoundary }) => <ErrorBoundaryPage resetErrorBoundary={resetErrorBoundary} />}
-    >
-      <Suspense fallback={<Loader />}>
-        <App />
-      </Suspense>
-    </ErrorBoundary>
+    <QueryClientProvider client={new QueryClient({ defaultOptions: { queries: { suspense: true } } })}>
+      <ReactQueryDevtools />
+      <QueryErrorResetBoundary>
+        {({ reset }) => (
+          <ErrorBoundary
+            onReset={reset}
+            fallbackRender={({ resetErrorBoundary }) => <ErrorBoundaryPage resetErrorBoundary={resetErrorBoundary} />}
+          >
+            <Suspense fallback={<Loader />}>
+              <App />
+            </Suspense>
+          </ErrorBoundary>
+        )}
+      </QueryErrorResetBoundary>
+    </QueryClientProvider>
     <ToastContainer
       css={ToastStyle}
       position="bottom-center"
