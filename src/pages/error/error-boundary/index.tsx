013a14bfbaaba04b60e8e78d6a9d5a2ce81cc099
yoonminsang <apdlyooapdl@naver.com>
2022-07-30 00:22:02 +0900

    feat: ErrorBoundary에 error props 추가

--- src/pages/error/error-boundary/index.tsx
+++ src/pages/error/error-boundary/index.tsx
@@ -4,9 +4,12 @@ import type { FC } from 'react';
 
 interface Props {
   resetErrorBoundary: (...args: unknown[]) => void;
+  error: Error;
 }
 
-const ErrorBoundaryPage: FC<Props> = ({ resetErrorBoundary }) => {
+const ErrorBoundaryPage: FC<Props> = ({ resetErrorBoundary, error }) => {
+  // TODO: 에러 관리(슬렉, amplitude 등등)
+  console.log('ErrorBoundaryPage error', error);
   return (
     <S.Wrapper>
       <h1>There was an error!</h1>
