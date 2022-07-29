38f19d9bc2cc457c1ed7e4294a4adb91a6ad90a7
yoonminsang <apdlyooapdl@naver.com>
2022-07-30 00:20:41 +0900

    feat: 공통 에러 유틸함수 생성

---
+++ src/utils/errors/index.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const errorHandler = {
+  status: {
+    401: () => toast.error('401 error'),
+    403: () => toast.error('403 error'),
+    409: () => toast.error('409 error'),
+  },
+  common: () => toast.error('common error'),
+};
+
+export const handleError = (err: unknown) => {
+  if (axios.isAxiosError(err)) {
+    const status = err.response?.status;
+    switch (status) {
+      case 401:
+      case 403:
+      case 409:
+        return errorHandler.status[status]();
+      default:
+        errorHandler.common();
+    }
+  }
+};
