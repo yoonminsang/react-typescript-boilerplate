881d687f7d2f0dc9d118374de1526d3db3bcbae1
yoonminsang <apdlyooapdl@naver.com>
2022-07-30 00:21:21 +0900

    wip: useQuery 옵션연습

--- src/hooks/queries/posts.ts
+++ src/hooks/queries/posts.ts
@@ -3,7 +3,17 @@ import { useQuery } from '@tanstack/react-query';
 import QUERIES_KEY from '@/constants/queries';
 import APIS from '@/apis';
 
-const useGetPosts = () => useQuery([QUERIES_KEY.POSTS.getPosts], APIS.POSTS.getPosts);
+const useGetPosts = () =>
+  useQuery([QUERIES_KEY.POSTS.getPosts], APIS.POSTS.getPosts, {
+    staleTime: 6000,
+    // cacheTime: 4000,
+    // onError: () => {
+    //   console.log('에러처리');
+    // },
+    // onSuccess: () => {
+    //   console.log('성공처리');
+    // },
+  });
 const useGetPostById = ({ id }: { id: number }) =>
   useQuery([QUERIES_KEY.POSTS.getPostById, id], () => APIS.POSTS.getPostById);
 
