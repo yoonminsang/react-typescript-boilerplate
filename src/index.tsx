783d5acf1d6d84b62ad7f446b26f06d1c6f8a3e3
yoonminsang <apdlyooapdl@naver.com>
2022-07-30 00:23:37 +0900

    feat: react query default 옵션 설정

--- src/index.tsx
+++ src/index.tsx
@@ -5,23 +5,44 @@ import { ErrorBoundary } from 'react-error-boundary';
 import { ToastContainer } from 'react-toastify';
 import { QueryClient, QueryClientProvider, QueryErrorResetBoundary } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import axios from 'axios';
 
 import App from './app';
 import { ToastStyle } from './styles/toast.style';
 import { Loader } from './components/common';
 import { GlobalStyle, theme } from './styles';
 import ErrorBoundaryPage from './pages/error/error-boundary';
+import { handleError } from './utils/errors';
 
 ReactDOM.createRoot(window.document.getElementById('root') as HTMLElement).render(
   <ThemeProvider theme={theme}>
     <GlobalStyle />
-    <QueryClientProvider client={new QueryClient({ defaultOptions: { queries: { suspense: true } } })}>
+    <QueryClientProvider
+      client={
+        new QueryClient({
+          defaultOptions: {
+            queries: {
+              retry: 1,
+              suspense: true,
+              useErrorBoundary(err) {
+                if (!axios.isAxiosError(err)) return true;
+                if (err.response?.status && err.response.status >= 500) return true;
+                return false;
+              },
+              onError: handleError,
+            },
+          },
+        })
+      }
+    >
       <ReactQueryDevtools />
       <QueryErrorResetBoundary>
         {({ reset }) => (
           <ErrorBoundary
             onReset={reset}
-            fallbackRender={({ resetErrorBoundary }) => <ErrorBoundaryPage resetErrorBoundary={resetErrorBoundary} />}
+            fallbackRender={({ resetErrorBoundary, error }) => (
+              <ErrorBoundaryPage resetErrorBoundary={resetErrorBoundary} error={error} />
+            )}
           >
             <Suspense fallback={<Loader />}>
               <App />
